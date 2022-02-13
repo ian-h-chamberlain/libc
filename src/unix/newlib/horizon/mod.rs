5926f9488e00d6d47b98251bf6d98f147ccc05c4
AzureMarker <mark.drobnak@gmail.com>
2022-02-12 19:59:00 -0800

    Replace pthread_getpriority nonstandard function with standard ones
    I chose the scheduler priorities based on
    https://man7.org/linux/man-pages/man7/sched.7.html
    
    I think the cooperative app cores are most like SCHED_FIFO, while the
    sys core is similar to SCHED_RR.
    
    However, I don't think our pthread implementation would be able to
    accurately return the right policy since we need to know what processor
    the thread is running on, and the only API to get that gets the ID for
    the current thread. Since the pthread function passes in a thread ID, we
    are unable to always get the processor ID and thus the policy.
    
    In this case, I think we should just always return (and accept in set)
    SCHED_FIFO. I don't think this will be used anyways.

--- src/unix/newlib/horizon/mod.rs
+++ src/unix/newlib/horizon/mod.rs
@@ -155,6 +155,10 @@ pub const RTLD_DEFAULT: *mut ::c_void = 0 as *mut ::c_void;
 pub const GRND_NONBLOCK: ::c_uint = 0x1;
 pub const GRND_RANDOM: ::c_uint = 0x2;
 
+// For pthread get/setschedparam
+pub const SCHED_FIFO: ::c_int = 1;
+pub const SCHED_RR: ::c_int = 2;
+
 // Horizon OS works doesn't or can't hold any of this information
 safe_f! {
     pub {const} fn WIFSTOPPED(_status: ::c_int) -> bool {
@@ -218,7 +222,17 @@ extern "C" {
         ideal_processor: ::c_int,
     ) -> ::c_int;
 
-    pub fn pthread_getpriority() -> ::c_int;
+    pub fn pthread_getschedparam(
+        native: ::pthread_t,
+        policy: *mut ::c_int,
+        param: *mut ::sched_param,
+    ) -> ::c_int;
+
+    pub fn pthread_setschedparam(
+        native: ::pthread_t,
+        policy: ::c_int,
+        param: *const ::sched_param,
+    ) -> ::c_int;
 
     pub fn getrandom(buf: *mut ::c_void, buflen: ::size_t, flags: ::c_uint) -> ::ssize_t;
 
